3ab1a9e8c4d490a118298efa4550707424dd4b53
JH_Kang <21900017@handong.ac.kr>
2023-01-19 13:44:20 +0900

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by junhyeok KANG on 2023/01/19.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
